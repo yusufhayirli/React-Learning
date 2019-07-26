7c84a0e189fa3c80f97f7b1204edda4f67aad79a
yusufhayirli <pensarnada@gmail.com>
2019-07-26 21:32:40 +0300

    binding with JSX

--- first-project/src/components/NewsForm.js
+++ first-project/src/components/NewsForm.js
@@ -4,13 +4,13 @@ class NewsForm extends React.Component{
     constructor(props){
         super(props);
     }
-    addButton = () => {
+    addButton(){
         this.props.addNews();
     };
     render(){
         return(
             <div>
-                <button onClick={this.addButton}>Add</button>
+                <button onClick={ () => this.addButton() }>Add</button>
             </div>
         )
     }
