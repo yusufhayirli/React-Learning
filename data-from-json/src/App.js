7484e2560d8a035d8bf8319b1f5766fdd99bff2e
yusufhayirli <pensarnada@gmail.com>
2019-07-27 23:57:48 +0300

    propTypes and CustomersList.css added.

--- data-from-json/src/App.js
+++ data-from-json/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CustomersList from './Post/CustomersList';
+import CustomersList from './components/CustomersList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <CustomersList/>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <CustomersList/>
+      </div>
+  )};
 }
 
 export default App;
---
+++ data-from-json/src/components/CustomersList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PostData from './MOCK_CUSTOMERS.json';
+import propTypes from 'prop-types';
+import './CustomersList.css';
+
+class CustomersList extends React.Component {
+    static propTypes = {
+        id: propTypes.number,
+        first_name: propTypes.string,
+        last_name: propTypes.string,
+        email: propTypes.string,
+        gender: propTypes.string,
+        phone: propTypes.string
+    }
+    render() {
+        return (
+            <div>
+                {PostData.map((CustomersDetails,index) => {
+                return (
+                <div className="data">
+                    <p>{"Customer's ID: " + CustomersDetails.id}</p>
+                    <p>{"First Name: " + CustomersDetails.first_name}</p>
+                    <p>{"Last Name: " + CustomersDetails.last_name}</p>
+                    <p>{"Email: " + CustomersDetails.email}</p>
+                    <p>{"Gender: " + CustomersDetails.gender}</p>
+                    <p>{"Phone: " + CustomersDetails.phone}</p>
+                    <hr/>
+                </div>
+                )})}
+            </div>
+        );
+    }
+}
+
+export default CustomersList;
