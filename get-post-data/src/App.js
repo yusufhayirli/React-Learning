8ccdc7bf9d36d7c9568dccb7553e509dd5524135
yusufhayirli <pensarnada@gmail.com>
2019-07-29 23:13:24 +0300

    Post data to url by input with a unique key

--- get-post-data/src/App.js
+++ get-post-data/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <PostList/>
+        <PostForm/>
       </div>
     );
   }
